7d48077d272102aa08d7363459b0899bb8afb175
womble <womble@DESKTOP-TL1DRU7>
2016-10-12 00:35:17 +0200

    Add project files.

---
+++ SimpleMVVMExample/ApplicationViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace SimpleMVVMExample
+{
+    public class ApplicationViewModel : ObservableObject
+    {
+        #region Fields
+
+        private ICommand _changePageCommand;
+
+        private IPageViewModel _currentPageViewModel;
+        private List<IPageViewModel> _pageViewModels;
+
+        #endregion
+
+        public ApplicationViewModel()
+        {
+            // Add available pages
+            PageViewModels.Add(new HomeViewModel());
+            PageViewModels.Add(new ProductsViewModel());
+
+            // Set starting page
+            CurrentPageViewModel = PageViewModels[0];
+        }
+
+        #region Properties / Commands
+
+        public ICommand ChangePageCommand
+        {
+            get
+            {
+                return _changePageCommand ?? (_changePageCommand = new RelayCommand(
+                           p => ChangeViewModel((IPageViewModel) p),
+                           p => p is IPageViewModel));
+            }
+        }
+
+        public List<IPageViewModel> PageViewModels => _pageViewModels ?? (_pageViewModels = new List<IPageViewModel>());
+
+        public IPageViewModel CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+            set
+            {
+                if (_currentPageViewModel == value) return;
+                _currentPageViewModel = value;
+                OnPropertyChanged("CurrentPageViewModel");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void ChangeViewModel(IPageViewModel viewModel)
+        {
+            if (!PageViewModels.Contains(viewModel))
+                PageViewModels.Add(viewModel);
+
+            CurrentPageViewModel = PageViewModels
+                .FirstOrDefault(vm => vm == viewModel);
+        }
+
+        #endregion
+    }
+}
